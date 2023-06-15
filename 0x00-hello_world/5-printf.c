8c170a79fae3059e6ea7af095996d95e4c37caad
oonixx <imrane.achaari@yahoo.com>
2023-06-15 05:21:47 -0700

    commit all

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Entry point of the program
+ * main - Entry point
  *
- * Return: 0 (Success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return 0;
-}
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 
+}
