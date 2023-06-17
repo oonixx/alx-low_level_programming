86067d2f86c7dcef44bd9bd185cdfff8640e2d4a
oonixx <imrane.achaari@yahoo.com>
2023-06-16 17:16:19 -0700

    commit this

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
+/**
+ * main - main function to generate a random number
+ *
+ * Return: Always 0 (Success)
+ *
+ */
 int main(void)
 {
 	int n;
