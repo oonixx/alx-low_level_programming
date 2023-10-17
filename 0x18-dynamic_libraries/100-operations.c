51e51db0c36ef0a8f765de1164c30a014ef2765b
root <imrane.achaari@yahoo.com>
2023-10-16 17:50:38 -0700

    the adv

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,50 @@
+/**
+ * add - addition
+ * @a: The number
+ * @b: The nuber
+ * Return: The result
+ */
+int add(int a, int b)
+{
+      return (a + b);
+}
+/**
+ * sub - addition
+ * @a: The number
+ * @b: The nuber
+ * Return: The result
+ */
+int sub(int a, int b)
+{
+      return (a - b);
+}
+/**
+ * mul - addition
+ * @a: The number
+ * @b: The nuber
+ * Return: The result
+ */
+int mul(int a, int b)
+{
+      return (a * b);
+}
+/**
+ * div - addition
+ * @a: The number
+ * @b: The nuber
+ * Return: The result
+ */
+int div(int a, int b)
+{
+      return (a / b);
+}
+/**
+ * mod - addition
+ * @a: The number
+ * @b: The nuber
+ * Return: The result
+ */
+int mod(int a, int b)
+{
+      return (a % b);
+}
